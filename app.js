9cd2d19baf9f8c6411a755b2b216721fdfe1d770
UsuallyChris <schoonc@gmail.com>
2019-05-02 18:47:12 -0700

    Added basic connection to MongoDB server

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const path = require('path');
 
 const app = express();
 
+// Mongoose
+mongoose.connect('mongodb://localhost:27017/cms', { useNewUrlParser: true} )
+  .then( response => {
+    console.log(`MongoDB connected`);
+  }).catch( err => {
+    console.log(`MongoDB connect error`)
+  });
+
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
