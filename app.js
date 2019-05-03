66680a81b39556c7ac4b11eb79bfc5c21189e00a
UsuallyChris <schoonc@gmail.com>
2019-05-02 18:53:05 -0700

    Added handlebars as dependancy

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
 // Mongoose
-mongoose.connect('mongodb://localhost:27017/cms', { useNewUrlParser: true} )
-  .then( response => {
+mongoose.connect('mongodb://localhost:27017/cms', { useNewUrlParser: true} ) // connect to local mongodb database cms 
+  .then( response => { // promise
     console.log(`MongoDB connected`);
-  }).catch( err => {
+  }).catch( err => { // catch error
     console.log(`MongoDB connect error`)
   });
 
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); // replaces bodyParser
+app.use(express.urlencoded({ extended: true })); // replaces bodyParser
+app.use(express.static(path.join(__dirname, 'public'))); // grab static files from __dirname/public/
 
 app.use('/', (req, res) => {
   res.send(`Home Page`);
